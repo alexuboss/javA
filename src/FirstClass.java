edd8e6e90d05b6ae835eb5ecb7ae8d92c315619c
alex <botnari@gmail.com>
2018-09-26 19:37:10 +0200

    26 septembrie

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,15 +1,23 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author user
  */
 public class FirstClass {
-    public static void main (String[] args){
-        System.out.println("Hello World!");
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String name = sc.nextLine();
+        System.out.println("String has been entered./n");
+        
+        System.out.println("sring entered" + name);
     }
 }
---
+++ src/secondstringday.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class secondstringday {
+
+    public static void main (String[] args) {
+//        String name = "Botnari";
+//        String surname = "Alexandru";
+//        String message = "My name is" + " " + name + " " + surname + "!";
+//        System.out.println(message);
+        double price = 2.6;
+        double tax = 2;
+        int quantity = 8;
+        double total = price * quantity * tax;
+        double message = total;
+        System.out.println("Total cost whith tax is" + " " + message);
+    }
+}
